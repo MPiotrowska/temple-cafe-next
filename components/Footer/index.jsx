789c92c3c6e0b803a862acc23647e378121f0dd9
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-12 17:46:23 +0100

    add footer

---
+++ components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import styles from '../../styles/Home.module.css';
+
+export const Footer = () => {
+  return (
+    <footer >
+      <a
+        href="https://google.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{' '}
+        <img src="" alt="Logo" />
+      </a>
+    </footer>
+  );
+};
