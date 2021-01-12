00c7f4c28a7b3d5fcbbc5a7319fb866e78e54d53
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-12 17:45:26 +0100

    create contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,24 @@
-import Form from '../components/Form'
 
-export default function Contact() {
-    return (
-        <Form/>
-    )
-  }
+import React from 'react';
+import { Container } from '../components/Container';
+
+const Contact = () => {
+  return <Container>Contact</Container>;
+};
+
+export default Contact;
+
+
+
+
+
+
+
+
+// import Form from '../components/Form'
+
+// export default function Contact() {
+//     return (
+//         <Form/>
+//     )
+//   }
