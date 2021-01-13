713cb53b695770a81f9e079b19579b090c8f058c
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-13 15:38:21 +0100

    refactor hero

--- components/Form.js
+++ components/Form.js
@@ -1,24 +1,24 @@
-const Form = () => {
-  return (
-    <form name="contact" action="/success" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label htmlFor="yourname">Your Name:</label> <br />
-        <input type="text" name="name" id="yourname" />
-      </p>
-      <p>
-        <label htmlFor="youremail">Your Email:</label> <br />
-        <input type="email" name="email" id="youremail" />
-      </p>
-      <p>
-        <label htmlFor="yourmessage">Message:</label> <br />
-        <textarea name="message" id="yourmessage"></textarea>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-  );
-};
+// const Form = () => {
+//   return (
+//     <form name="contact" action="/success" method="POST" data-netlify="true">
+//       <input type="hidden" name="form-name" value="contact" />
+//       <p>
+//         <label htmlFor="yourname">Your Name:</label> <br />
+//         <input type="text" name="name" id="yourname" />
+//       </p>
+//       <p>
+//         <label htmlFor="youremail">Your Email:</label> <br />
+//         <input type="email" name="email" id="youremail" />
+//       </p>
+//       <p>
+//         <label htmlFor="yourmessage">Message:</label> <br />
+//         <textarea name="message" id="yourmessage"></textarea>
+//       </p>
+//       <p>
+//         <button type="submit">Send</button>
+//       </p>
+//     </form>
+//   );
+// };
 
-export default Form;
+// export default Form;
