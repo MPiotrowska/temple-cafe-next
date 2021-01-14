d19ceaee786e1e571dadade9958c75f6036e7a23
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-14 13:13:48 +0100

    add hero logic

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import Link from 'next/link';
+import { InView } from 'react-intersection-observer';
+import {  Container} from "../Container";
+
+
+import { useScrollDispatch } from '../../lib/scrollContext';
+
+// styles
+// import styles from '../../styles/home.module.css';
+import styles from './hero.module.css'
+import containerStyles from '../Container/container.module.css';
+
 
 export const Hero = () => {
-  return <div className={styles.hero}></div>;
-};
+  const dispatch = useScrollDispatch();
+  const handleChange = (inView) => {
+    const action = { type: 'scroll', payload: inView };
+    dispatch(action);
+  };
+
+  return   (
+
+  <Container fullWidth style={{ position: 'relative' }}>
+  <InView as="div" onChange={handleChange}>
+    <img className={styles.heroImage} src="/img/hero.jpg" alt="coffee" />
+  </InView>
+  <Container className={containerStyles.heroText}>
+    <Container className={containerStyles.heroStandard}>
+     
+      {/* <Link href="/contact">
+        <a className={styles.buttonLink}>Contact</a>
+      </Link> */}
+    </Container>
+  </Container>
+</Container>)}
