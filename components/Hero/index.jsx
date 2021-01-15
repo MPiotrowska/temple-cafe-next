43cdd9fbaeae7995dc49da223a9b3bc7b88e0c2f
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-15 16:56:38 +0100

    refactor hero

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,35 +1,33 @@
-import React from 'react';
-import Link from 'next/link';
-import { InView } from 'react-intersection-observer';
-import {  Container} from "../Container";
+import React from "react";
+import Link from "next/link";
+import { InView } from "react-intersection-observer";
+import { Container } from "../Container";
 
-
-import { useScrollDispatch } from '../../lib/scrollContext';
+import { useScrollDispatch } from "../../lib/scrollContext";
 
 // styles
-import styles from "./hero.module.css"
-import containerStyles from '../Container/container.module.css';
+import styles from "./hero.module.css";
 
 
 export const Hero = () => {
   const dispatch = useScrollDispatch();
   const handleChange = (inView) => {
-    const action = { type: 'scroll', payload: inView };
+    const action = { type: "scroll", payload: inView };
     dispatch(action);
   };
 
-  return   (
-
-  <Container fullWidth style={{ position: 'relative' }}>
-  <InView as="div" onChange={handleChange}>
-    <img className={styles.heroImage} src="/img/hero.jpg" alt="coffee" />
-  </InView>
-  <Container className={styles.heroText}>
-    <Container className={styles.heroStandard}>
-     
-      <Link href="/contact">
-        <a className={styles.buttonLink}>Contact</a>
-      </Link>
+  return (
+    <Container fullWidth style={{ position: "relative" }}>
+      <InView as="div" onChange={handleChange}>
+        <img className={styles.heroImage} src="/img/hero.jpg" alt="coffee" />
+      </InView>
+      <Container className={styles.heroText}>
+        <Container className={styles.heroStandard}>
+          <Link href="/contact">
+            <a className={styles.buttonLink}>Contact</a>
+          </Link>
+        </Container>
+      </Container>
     </Container>
-  </Container>
-</Container>)}
+  );
+};
