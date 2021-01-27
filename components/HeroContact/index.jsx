cd447d647a54a16fe78f050257365ff852f06d61
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-27 18:54:20 +0100

    refactor hero contact

--- components/HeroContact/index.jsx
+++ components/HeroContact/index.jsx
@@ -1,53 +1,18 @@
 import React from 'react'
 import { Container } from '../Container'
 import styles from './heroContact.module.css'
+import containerStyles from '../Container/container.module.css'
 
 export const HeroContact = () => {
     return (
-      <Container className="half">
+      <Container className={containerStyles.half}>
 
         <h1
-          // className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen has-unique-background "
+          
           className={styles.hasUniqueBackground }> Whatever
-        {/* //   style={{
-        //     // boxShadow:
-        //     //   'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-        //     // // backgroundColor: 'rgb(255, 68, 0)',
-           
-        //   }}
-        // >
-        //   {title} */}
+        
         </h1>
-        {/* <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            // boxShadow:
-            //   'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            // backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-            textAlign: 'center',
-          }}
-        >
-          {subheading}
-        </h3>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            // boxShadow:
-            //   'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            // backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-            textAlign: 'center',
-          }}
-        >
-          {optionalText}
-        </h3>
-
-       <Button href="/contact" text="Contact"/> */}
+        
       </Container>
     
     )
