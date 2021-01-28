d38194c016d394376b0db6f5332b1140cd1d5932
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-28 12:02:17 +0100

    reactor hero contact

--- components/HeroContact/index.jsx
+++ components/HeroContact/index.jsx
@@ -10,7 +10,10 @@ import containerStyles from "../Container/container.module.css";
 export const HeroContact = () => {
   return (
     <Container className={containerStyles.half} alignment="right">
-      <h1 className={styles.hasUniqueBackground}> Whatever</h1>
+      <h1 className={styles.hasUniqueBackground}> Temple Café</h1>
+      <h3 className={styles.address}>815 London Rd, Southend-on-Sea,
+Westcliff-on-Sea SS0 9SY</h3>
+
 
       <Link href="/contact">
         <a className={styles.buttonLink}>Contact</a>
