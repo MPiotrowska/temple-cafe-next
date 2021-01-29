f884e48e590f9c358618867096fe8aea49660bfe
MPiotrowska <monikapiotrowska27@gmail.com>
2021-01-29 13:17:55 +0100

    refactor contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,24 +1,19 @@
+import React from "react";
+import { Container } from "../components/Container";
+import { Form } from "../components/Form/Form";
 
-import React from 'react';
-import { Container } from '../components/Container';
+import containerStyles from "../components/Container/container.module.css";
+
 
 const Contact = () => {
-  return <Container>Contact</Container>;
+  return (
+    <Container>
+      <Container>
+       
+     
+      </Container>
+    </Container>
+  );
 };
 
 export default Contact;
-
-
-
-
-
-
-
-
-// import Form from '../components/Form'
-
-// export default function Contact() {
-//     return (
-//         <Form/>
-//     )
-//   }
