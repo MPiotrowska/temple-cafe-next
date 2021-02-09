aa7418e09184e35a85f84c5272bc634509c90d69
MPiotrowska <monikapiotrowska27@gmail.com>
2021-02-09 12:29:28 +0100

    change font

--- pages/_document.js
+++ pages/_document.js
@@ -15,8 +15,10 @@ class MyDocument extends Document {
           <meta charSet="utf-8" />
           <link rel="icon" href="/favicon.ico" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
-            rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap" rel="stylesheet"
+
+            // href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+            // rel="stylesheet"
           />
         </Head>
         <body>
