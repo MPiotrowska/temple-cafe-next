5bf768cd57829e1e2940de91bd784e7cfca404f0
MPiotrowska <monikapiotrowska27@gmail.com>
2021-02-12 16:23:30 +0100

    refactor home about

--- components/HomeAbout/index.jsx
+++ components/HomeAbout/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Container } from '../Container'
 import containerStyles from '../Container/container.module.css'
+import styles from './homeAbout.module.css'
 
 
 export const HomeAbout = () => {
     return (
        <Container flex>
-           <Container className={containerStyles.fortyPercent}>rgehrehthtrfh</Container>
+           <Container className={containerStyles.fortyPercent}>
+               <span className={styles.greeting}>Welcome</span>
+               <h1>sdgvdsgb</h1>
+               <p className={styles.about}>svfsdg</p>
+               </Container>
            <Container className={containerStyles.half}>rfhrhtrhtrht</Container>
        </Container>
     )
